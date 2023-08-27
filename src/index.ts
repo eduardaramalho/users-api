5b237b6cf2eb06d21d39a5119c7d4db767935637
Eduarda Ramalho <eduardacns.ramalho@gmail.com>
2023-08-27 12:24:00 -0300

    feat: add CreatUserController with validation

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,14 @@ import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
+import { CreateUserController } from "./controllers/create-user/create-user";
 
 const main = async () => {
   config();
   const app = express();
+
+  app.use(express.json());
 
   await MongoClient.connect();
 
@@ -16,12 +20,26 @@ const main = async () => {
 
     const { body, statusCode } = await getUsersController.handle();
 
-    res.send(body).status(statusCode);
+    res.status(statusCode).send(body);
   });
 
   const port = process.env.PORT || 8000;
 
   app.listen(port, () => console.log(`listening on port ${port}!`));
+
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
+
+    res.status(statusCode).send(body);
+  });
 };
 
 main();
